4ed63453bf4a555a586b71c9e55e2ca2a1ca6c3c
Pranada6467 <112790949+Pranada6467@users.noreply.github.com>
2024-10-19 22:57:37 +0530

    Add files via upload

---
+++ object_detection.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+
+class ObjectDetection:
+    def __init__(self, weights_path="dnn_model/yolov4.weights", cfg_path="dnn_model/yolov4.cfg"):
+        print("Loading Object Detection")
+        print("Running opencv dnn with YOLOv4")
+        self.nmsThreshold = 0.4
+        self.confThreshold = 0.5
+        self.image_size = 608
+
+        # Load Network
+        net = cv2.dnn.readNet(weights_path, cfg_path)
+
+        # Enable GPU CUDA
+        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+        self.model = cv2.dnn_DetectionModel(net)
+        self.classes = []
+        self.load_class_names()
+        self.colors = np.random.uniform(0, 255, size=(80, 3))
+        self.model.setInputParams(size=(self.image_size, self.image_size), scale=1/255)
+
+        # KLT tracker parameters
+        self.lk_params = dict(winSize=(15, 15),
+                              maxLevel=2,
+                              criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        self.feature_params = dict(maxCorners=100,
+                                   qualityLevel=0.3,
+                                   minDistance=7,
+                                   blockSize=7)
+        self.tracks = []
+        self.track_len = 10
+        self.detect_interval = 5
+        self.frame_idx = 0
+
+    def load_class_names(self, classes_path="dnn_model/classes.txt"):
+        with open(classes_path, "r") as file_object:
+            for class_name in file_object.readlines():
+                class_name = class_name.strip()
+                self.classes.append(class_name)
+        self.colors = np.random.uniform(0, 255, size=(80, 3))
+        return self.classes
+
+    def detect(self, frame):
+        return self.model.detect(frame, nmsThreshold=self.nmsThreshold, confThreshold=self.confThreshold)
+
+    def update_tracks(self, frame, prev_frame):
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+
+        if len(self.tracks) > 0:
+            img0, img1 = prev_gray, frame_gray
+            p0 = np.float32([tr[-1] for tr in self.tracks]).reshape(-1, 1, 2)
+            p1, _st, _err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **self.lk_params)
+            p0r, _st, _err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **self.lk_params)
+            d = abs(p0-p0r).reshape(-1, 2).max(-1)
+            good = d < 1
+            new_tracks = []
+            for tr, (x, y), good_flag in zip(self.tracks, p1.reshape(-1, 2), good):
+                if not good_flag:
+                    continue
+                tr.append((x, y))
+                if len(tr) > self.track_len:
+                    del tr[0]
+                new_tracks.append(tr)
+            self.tracks = new_tracks
+
+        return self.tracks
